70d7d06a016f7c202dcc4d2f231c90b5aab88797
niccolassmdrz <nicolas.robles.riaza@alumnos.upm.es>
2025-05-19 17:33:02 +0200

    Update Main.java
    push

--- src/series/Main.java
+++ src/series/Main.java
@@ -1,16 +1,38 @@
 package series;
 
 public class Main {
-    public static void main(String[] args) {
-        // Crea el gestor de conexiones
-        ConnectionManager cm = new ConnectionManager("estudiante", "bbdd-upm");
 
-        // Crear las tareas:
-        DataBaseTask[] tasks = { new CreateTableValora(), new CreateTableComenta() };
-        String[] data = { "", "" };
+    public static void testInsertaTemporadasConAC() {
+        // Crea el gestor de conexiones
+        ConnectionManager cm = new ConnectionManager("alumno","bbdd-upm");
+
+        // Crear las tareas
+        // Cambia los archivos para hacer mas pruebas
+        DataBaseTask[] tasks = { new InsertaTemporadas(), new InsertaTemporadas() };
+        String[] data = { "data/temporadas.csv", "data/temporadas_duplicada.csv" };
 
         // Llamar a run:
         String ok = cm.runTask(tasks, data);
         System.out.println(ok);
+    }
+
+    public static void testInsertaTemporadasSinAC() {
+        // Crea el gestor de conexiones
+        ConnectionManager cm = new ConnectionManagerWithoutAC("estudiante","bbdd-upm");
+
+        // Crear las tareas
+        // Cambia los archivos para hacer mas pruebas
+        DataBaseTask[] tasks = { new InsertaTemporadas(), new InsertaTemporadas() };
+        String[] data = { "data/temporadas.csv", "data/temporadas_duplicada.csv" };
+
+        // Llamar a run:
+        String ok = cm.runTask(tasks, data);
+        System.out.println(ok);
+    }
+
+    public static void main(String[] args) {
+        // Comenta o descomenta para probar de distintas maneras
+        testInsertaTemporadasConAC();
+        // testInsertaTemporadasSinAC();
     }
 }
